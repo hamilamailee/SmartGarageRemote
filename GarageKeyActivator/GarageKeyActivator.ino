83a166a82c105c5c7cb96babcecbf9f46d24978e
Saee <saee_saadat@outlook.com>
2023-02-05 15:21:49 +0330

    added GarageKeyActivator (the extra point part) arduino code but its not complete yet

--- GarageKeyActivator/GarageKeyActivator.ino
+++ GarageKeyActivator/GarageKeyActivator.ino
@@ -11,9 +11,8 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 char *device_id;
-char *access_token;
-char *SUB_TOPIC = "/garage";
-char *PUB_TOPIC = "/messages";
+char *PUB_TOPIC = "/garage";
+char *pub_topic = (char *)malloc(128);
 char *MQTT_HOST = "addd6ec8.us-east-1.emqx.cloud";
 int MQTT_PORT = 15345;
 char *mqttUsername = "arduino";
@@ -26,30 +25,25 @@ bool has_ssid_pass_really = true;
 bool connected_to_wifi = false;
 bool connected_to_mqtt = false;
 
-const char *ACCESS_POINT_SSID = "parking_remote";
+int keyPin = D1;
+int lightPin = D2;
+int signalId = 1;
+
+const char *ACCESS_POINT_SSID = "parking_remote_key";
 const char *ACCESS_POINT_PASS = "testtest";
 IPAddress local_ip(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 AsyncWebServer server(8080);
 
-int doorPin = D0;
-int checkPin = D1;
-
-long startTime = 0;
-long lastMetricSent = 0;
-
 void setup()
 {
   ESP.eraseConfig();
   Serial.begin(9600);
   delay(10);
-  Serial.println("V1.0");
-  //  setupAP();
-  //  setupServer();
   connectToWifi();
-  pinMode(doorPin, OUTPUT);
-  pinMode(checkPin, OUTPUT);
+  pinMode(keyPin, INPUT);
+  pinMode(lightPin, OUTPUT);
   Serial.println("Setup done");
 }
 
@@ -61,12 +55,7 @@ void setupAP()
   Serial.print("Setup AP ");
   Serial.println(WiFi.softAP(ACCESS_POINT_SSID, ACCESS_POINT_PASS) ? "Successful" : "Failed!");
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address");
-  Serial.println(IP);
-  Serial.println("Setup wifi done");
-  Serial.println(getCharArrayFromString(WiFi.macAddress()));
-  Serial.println();
-  access_token = device_id = getCharArrayFromString(WiFi.macAddress());
+  Serial.println("Access point setup done");
 }
 
 bool connectToWifi()
@@ -93,72 +82,31 @@ bool connectToWifi()
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
   device_id = getCharArrayFromString(WiFi.macAddress());
-  //  device_id = "A4:CF:12:F0:00:B3";
-  access_token = device_id;
   Serial.printf("MacAddress: %s\n", device_id);
   return true;
-}
-
-void callback(char *topic, byte *payload, unsigned int length)
-{
-  Serial.printf("Call back %s %s \n", topic, payload);
-  DynamicJsonDocument data(length + 10);
-  DeserializationError err = deserializeJson(data, payload);
-  if (err)
-  {
-    Serial.print(("deserializeJson() failed: "));
-  }
-  if (!data.containsKey("mode") || !data.containsKey("id")) {
-    sendError("Invalid json received thus ignored");
-  }
-  const char* mode = data["mode"];
-  Serial.println("This is the mode you sent me dude!");
-  Serial.println(mode);
-  Serial.println("learn");
-  int id = data["id"];
-  if (strcmp(mode, "learn") == 0) {
-    learn(id);
-  } else {
-    operate(id);
-  }
-  return;
 }
 
 bool connectToMqtt()
 {
   client.setServer(MQTT_HOST, MQTT_PORT);
-  client.setCallback(callback);
   if (!client.connect(device_id, mqttUsername, mqttPassword))
   {
     Serial.println("Failed to connect to mqtt!");
     delay(1000);
     return false;
   }
-  client.subscribe(SUB_TOPIC);
-  //  Serial.println("Connected to mqtt");
   return true;
 }
 
-void sendMessage(char* message)
+void sendCommand(int id)
 {
   DynamicJsonDocument doc(2048);
-  doc["mode"] = "message";
-  doc["message"] = message;
+  doc["mode"] = "transmit";
+  doc["id"] = id;
   char buf[2048];
   serializeJson(doc, buf);
   client.publish(PUB_TOPIC, buf);
-  Serial.println("Sent message");
-}
-
-void sendError(char* message)
-{
-  DynamicJsonDocument doc(2048);
-  doc["mode"] = "error";
-  doc["message"] = message;
-  char buf[2048];
-  serializeJson(doc, buf);
-  client.publish(PUB_TOPIC, buf);
-  Serial.println("Sent message");
+  Serial.println("Sent command");
 }
 
 
@@ -172,11 +120,6 @@ char *getCharArrayFromString(String str)
 
 void setupServer()
 {
-  server.on("/ping", HTTP_GET, [](AsyncWebServerRequest * request)
-  {
-    request->send(200, "text/plain", "PONG");
-  });
-
   server.on("/connect", HTTP_POST, [](AsyncWebServerRequest * request)
   {
     if (request->hasArg("ssid") && request->hasArg("pass") && request->arg("ssid") != NULL && request->arg("pass") != NULL) {
@@ -184,11 +127,7 @@ void setupServer()
       pass = getCharArrayFromString(request->arg("pass"));
       char* data = (char*)malloc(1024);
       data[0] = '\0';
-      strcat(data, "{\"device_id\":\"");
-      strcat(data, device_id);
-      strcat(data, "\",\"access_token\":\"");
-      strcat(data, access_token);
-      strcat(data, "\"}");
+      strcat(data, "Got wifi info!\0");
       request->send(200, "text/plain", data);
       has_ssid_pass = true;
       connected_to_wifi = false;
@@ -224,32 +163,6 @@ void setupServer()
   Serial.println("Setup server done");
 }
 
-void learn(int id)
-{
-  Serial.print("Learning for id = ");
-  Serial.println(id);
-  if (id < 1 || id > 5) {
-    sendError("id must be between 1 and 5");
-  } else {
-    // TODO: do the actual learning
-    // TODO: save in memory
-    sendMessage("Learned");
-  }
-}
-
-void operate(int id)
-{
-  Serial.print("Transmitting for id = ");
-  Serial.println(id);
-
-  if (id < 1 || id > 5) {
-    sendError("id must be between 1 and 5");
-  } else {
-    // TODO: read from memory
-    // TODO: do the actual transmitting
-    sendMessage("Sent");
-  }
-}
 
 void loop()
 {
@@ -270,5 +183,14 @@ void loop()
   {
     Serial.println("mqtt disconnected");
     connected_to_mqtt = false;
+  }
+
+  if (digitalRead(keyPin) == HIGH) {
+    Serial.println("Transmission begin");
+    sendCommand(signalId);
+    digitalWrite(lightPin, HIGH);
+    delay(1000);
+    digitalWrite(lightPin, LOW);
+    Serial.println("Transmission complete");
   }
 }
